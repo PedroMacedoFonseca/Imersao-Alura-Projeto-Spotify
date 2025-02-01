2418d065c92de4945b191fa6a322273880350917
unknown <pedromacedofonseca@gmail.com>
2025-02-01 17:47:21 -0300

    Final de Projeto

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const searchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById("result-artist");
+const resultArtist = document.getElementById('result-artist');
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists`; 
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result))
+        .then((data) => {
+            const result = data.artists.filter(artist => artist.name.toLowerCase() === searchTerm.toLowerCase());
+            displayResults(result.length > 0 ? result[0] : {}); 
+        });
 }
 
 function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
+    resultPlaylist.classList.add('hidden')
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
@@ -24,12 +27,13 @@ function displayResults(result) {
 
 document.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
+    if (searchTerm != '') {
         resultPlaylist.classList.add('hidden');
         resultArtist.classList.remove('hidden');
+        var result = requestApi(searchTerm);
+        displayResults(result)        
         return
     }
-    
-    requestApi(searchTerm);
+       
 })
 
